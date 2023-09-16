0799730b4b589df2afaed0dd22529bdb83fb7696
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2023-09-16 20:59:14 +0530

    Update broadcast.py

--- broadcast.py
+++ broadcast.py
@@ -26,24 +26,35 @@ async def broadcast(session, text, time):
               if dialog.chat.permissions is not None and dialog.chat.permissions.can_send_messages:
               	list.append(dialog.chat.id)
            print(list)
+           if len(list) > 300:
+               return f"There is total {len(list)} which is greater than 300\nI Only Support upto 300 group so leave some group"
            while True:
            	for i in list:
-			try:
-				await owo.send_message(int(i), text)
-           		        if i < 25:
-					await asyncio.sleep(1)
-           		        if i < 50 and i > 25:
-           			        await asyncio.sleep(2)
-           		        if i < 100 and i > 50:
-           			        await asyncio.sleep(5)
-           		        if i < 200 and i > 100:
-           			        await asyncio.sleep(10)
+                    try:
+                        await owo.send_message(int(i), text)
+                        if i < 5:
+                            await asyncio.sleep(1)
+                        if i < 10 and i > 5:
+                             await asyncio.sleep(10)
+                        if i < 50 and i > 10:
+                             await asyncio.sleep(15)
+                        if i < 80 and i > 50:
+                             await asyncio.sleep(20)
+	                if i < 100 and i > 80:
+                            await asyncio.sleep(1)
+                        if i < 120 and i > 100:
+                             await asyncio.sleep(10)
+                        if i < 150 and i > 120:
+                             await asyncio.sleep(15)
+                        if i < 300 and i > 150:
+                             await asyncio.sleep(20)
 			except FloodWait as x:
 				print(f"Flood Wait Second of : {x.value}")
 				await asycio.sleep(x.value)
 			except:
 				pass
            	await asyncio.sleep(int(time))
+    return "Done ✅"
           
              	    
               
@@ -55,8 +66,8 @@ async def bc(client, message):
 	text = await client.ask(chat_id, "Give me text")
 	time = await client.ask(chat_id, "Give me time in secon")
 	await message.reply_text("Processing....")
-	await broadcast(my_session.text, text.text, time.text)
-	await message.reply_text("Done")
+	lol = await broadcast(my_session.text, text.text, time.text)
+	await message.reply_text(f{lol}")
 
 
 app.run()
